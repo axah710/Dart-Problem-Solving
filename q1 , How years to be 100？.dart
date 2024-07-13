43097d5128ebbe499971e91ba9ea62e8f9f5e63b
axah710 <ahmed36085361@gmail.com>
2024-07-13 11:41:28 +0300

    added user prompt for name and age, calculated years to 100

--- q1 , How years to be 100？.dart
+++ q1 , How years to be 100？.dart
@@ -1,5 +1,20 @@
 import 'dart:io';
 
+/// This code prompts the user for their name and age, and then calculates the
+/// number of years until they are 100 years old.
+///
+/// The code uses a do-while loop to prompt the user for their name until a
+/// non-null, non-empty string is provided.
+///
+/// It then uses another do-while loop to prompt the user for their age,
+///  handling various error conditions such as age being greater than 100,
+/// exactly 100, or less than 0.
+///
+/// Once a valid age is entered, the code calculates the number of years until
+/// the user is 100 and prints this information.
+///
+/// Finally, the code schedules a delayed operation that prints a message and
+/// exits the application after 3 seconds.
 void main() {
   String? name;
   String? ageInput;
@@ -8,6 +23,9 @@ void main() {
     print("What Is Your Name");
     name = stdin.readLineSync();
   } while (name == null || name.isEmpty);
+  // This do-while loop prompts the user for their name and keeps asking until
+  //a non-null, non-empty string is provided. stdin.readLineSync() reads a line
+  //of text from the standard input.
   do {
     print("What is your age?");
     ageInput = stdin.readLineSync();
@@ -34,6 +52,16 @@ void main() {
       }
     }
   } while (age == null || age < 0 || age >= 100);
+// This do-while loop handles age input:
+// Prompts the user for their age.
+// Reads the input and tries to parse it as an integer.
+// Checks for various conditions:
+// If age is greater than 100, prompts for a valid number.
+// If age is exactly 100, acknowledges the user's age.
+// If age is less than 0, prompts for a valid positive age.
+// If parsing fails (due to a non-numeric input), it catches the exception and
+//asks for a valid number again.
+// The loop continues until a valid age (0-99) is entered.
   print(
     "Your name is $name and you have ${100 - age} years to be 100 years old.",
   );
@@ -46,4 +74,9 @@ void main() {
     );
     exit(0);
   });
+//  This block of code schedules a delayed operation:
+// Future.delayed creates a future that completes after a specified duration
+// (3 seconds in this case).
+// After the delay, it prints a message indicating the app will terminate and
+// then exits the application using exit(0).
 }
