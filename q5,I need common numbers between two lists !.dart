27341ba3128be2c0d03c362ef27abd48b6c54695
axah710 <ahmed36085361@gmail.com>
2024-07-13 11:47:55 +0300

    (Added function to find common elements between two lists)

--- q5,I need common numbers between two lists !.dart
+++ q5,I need common numbers between two lists !.dart
@@ -1,3 +1,31 @@
+/// Finds the common elements between two lists of integers.
+///
+/// This function first converts the two input lists into sets, which removes any
+/// duplicate values. It then calculates the intersection of the two sets,
+/// which gives the common elements between the two lists. Finally, it converts
+/// the resulting set back into a list and returns it.
+///
+/// Example usage:
+///
+/// List<int> firstList = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10];
+/// List<int> secondList = [1, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 19];
+/// List<int> commonElements = findCommonElements(firstList, secondList);
+/// print(commonElements); // Output: [1, 3, 5, 7, 9]
+///
+///
+/// @param firstListOfNumbers The first list of integers.
+/// @param secondListOfNumbers The second list of integers.
+/// @return A list of the common elements between the two input lists.
+List<int> findCommonElements(
+    List<int> firstListOfNumbers, List<int> secondListOfNumbers) {
+  return firstListOfNumbers
+      .toSet()
+      .intersection(
+        secondListOfNumbers.toSet(),
+      )
+      .toList();
+}
+
 main() {
   List<int> firstListOfNumbers = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10];
   // Explanation: This line creates a list named firstListOfNumbers and
